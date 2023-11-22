943e73bec63d18fe7e34574ae19bb9b2cc72a569
Brianmanoti <brianmanoti254@gmail.com>
2023-11-22 12:35:19 +0000

    first node Program

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,34 @@
-process.stdout.write("welcome to Holberton School, What is your name?\n")
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
-	const name = process.stdin.read();
-
-	if (name !== null) {
-		process.stdout.write(`Your name is: ${name}`)
-	}
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
 
-process.stdin.on('end' () => {
-	process.stdout.write('This important software is now closing\n')
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
+
+// const readline = require('readline');
+
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+
+// console.log('Welcome to Holberton School, what is your name?');
+
+// rl.on('line', (input) => {
+//   if (name.toLowerCase() === 'exit') {
+//     console.log('This important sofware is now closing');
+//     rl.close();
+//   } else {
+//     console.log('Your name is: ${name}');
+//   }
+// });
+
+// rl.on('close', () => {
+//   console.log('This important software is now closing');
+// });
